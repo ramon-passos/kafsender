f18d340513da9ac6f6a170f4b3d4de8f34fbff7a
ramon-passos <ramonpassos2011@gmail.com>
2024-10-22 13:46:03 -0300

    refact: improve consumer code

--- kafka/sample-consumer.js
+++ kafka/sample-consumer.js
@@ -6,17 +6,20 @@ const consumer = kafka.consumer({
   sessionTimeout: 10000,
 })
 
+function consumeMessage({ message, topic, partition }) {
+  console.log("Consuming My message:")
+  console.log({
+    value: message.value.toString(),
+    topic,
+    partition,
+    offset: message.offset,
+  });
+}
+
 consumer.connect().then(async () => {
   await consumer.subscribe({ topic: 'sample-topic' });
   await consumer.run({
     autoCommit: true,
-    eachMessage: async ({ topic, partition, message }) => {
-      console.log({
-        value: message.value.toString(),
-        topic,
-        partition,
-        offset: message.offset,
-      });
-    },
+    eachMessage: consumeMessage,
   });
 })
